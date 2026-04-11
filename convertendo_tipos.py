81a7f905dcea0d15d9062de199885712adbc53f1
Cinthia Alves de Souza <rp.cinthia@yahoo.com.br>
2026-04-11 17:45:06 -0300

    python_inicial_bootcamp
    aulas do bootcamp DIO

---
+++ convertendo_tipos.py
@@ -0,0 +1,13 @@
+print(int(1.97348728)) # converte o número 1.97348728 para um número inteiro, resultando em 1
+print(int("10")) # converte a string "10" para um número inteiro, resultando em 10
+print(float("10.10")) # converte a string "10.10" para um número de ponto flutuante, resultando em 10.10
+print(float(100)) # converte o número inteiro 100 para um número de ponto flutuante, resultando em 100.0
+
+valor = 10
+valor_str = str(valor) # converte o número inteiro 10 para uma string, resultando em "10"
+print(type(valor)) # imprime o tipo da variável valor, que é <class 'int'>
+print(type(valor_str)) # imprime o tipo da variável valor_str, que é <class 'str'>  
+print(type(str(10.10)))  
+
+print(100 / 2) # resultado é 50.0, pois a divisão de dois números inteiros resulta em um número de ponto flutuante
+print(100 // 2) # resultado é 50, pois a divisão inteira de dois números inteiros resulta em um número inteiro
---
+++ variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = "Cinthia"  # variável, pode ser alterada, por convenção, é escrita em letras minúsculas
+idade = 46
+
+nome, idade = "jacqueline", 30 # atribuição múltipla, onde o valor da variável nome é "jacqueline" e o valor da variável idade é 30
+
+print(nome, idade)
+
+limite_saque_diario = 1000.00
+print(limite_saque_diario) #SNAKE CASE, por convenção, é escrita em letras minúsculas e as palavras são separadas por underline
+
+BRAZILIAN_STATES = ["SP", "RJ", "MG", "ES"] # constante, não pode ser alterada, por convenção, é escrita em letras maiúsculas
+print(BRAZILIAN_STATES)
